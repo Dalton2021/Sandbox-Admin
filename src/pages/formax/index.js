15536d0ce4ec2a1d7b0e0d1e09e3a27f2e879c50
Dalton2021 <dabner@clutch-inc.com>
2025-02-12 15:14:28 -0500

    control form

--- src/pages/formax/index.js
+++ src/pages/formax/index.js
@@ -1,30 +1,52 @@
 import { Formax } from '@clutch-inc/usfa-ui-admin';
 import { Button, Col, Container, Row } from 'react-bootstrap';
-import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import HomeBtn from '@/components/HomeBtn';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
 
 export default function Home() {
+  const validationSchema = z.object({
+    username: z
+      .string()
+      .min(3, { message: 'Username must be at least 3 characters' })
+      .nonempty({ message: 'Username is required' }),
+  });
+
   const methods = useForm({
-    defaultValues: { username: '', age: '', password: '' },
-    resolver: zodResolver(),
+    defaultValues: {
+      username: '',
+    },
+    resolver: zodResolver(validationSchema),
   });
 
   return (
     <>
       <Container className="mt-4">
         <h1>Formax tests</h1>
-        <Row>
+        <Row className="mt-4">
           <Col>
-            <Formax {...{ methods }} onSubmit={(data) => console.log('Submitted', data)}>
-              <Formax.Control name="username" label="Username" />
+            <h2>Control</h2>
+            <Formax {...{ methods }} onSubmit={(data) => alert(`Submitted username: ${data.username}`)}>
+              <Formax.Control name="username" label="Username" required />
               <Button type="submit" className="mt-3">
                 Submit
               </Button>
             </Formax>
-            <HomeBtn />
           </Col>
         </Row>
+        <Row className="mt-4">
+          <Col>
+            <h2>Control</h2>
+            <Formax {...{ methods }} onSubmit={(data) => alert(`Submitted username: ${data.username}`)}>
+              <Formax.Control name="username" label="Username" required />
+              <Button type="submit" className="mt-3">
+                Submit
+              </Button>
+            </Formax>
+          </Col>
+        </Row>
+        <HomeBtn hideContainer />
       </Container>
     </>
   );
